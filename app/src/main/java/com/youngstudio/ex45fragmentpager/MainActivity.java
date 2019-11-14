5b061198872c5d62695d63b4cf0601baf346124c
rmflawk <ant921111@naver.com>
2019-11-14 17:33:38 +0900

    1

---
+++ app/src/main/java/com/youngstudio/ex45fragmentpager/MainActivity.java
@@ -0,0 +1,25 @@
+package com.youngstudio.ex45fragmentpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager= findViewById(R.id.pager);
+        adapter= new MyAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
+    }
+
+}
